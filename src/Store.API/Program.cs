deb9920d8321a21d17ca63aaaeab4932bf21b125
Andre Furlan <furlan.andre91@gmail.com>
2026-05-19 15:22:35 -0400

    chore(ambiente): Adicionado no program configuração do banco e exposto o uso de controllers

--- src/Store.API/Program.cs
+++ src/Store.API/Program.cs
@@ -1,6 +1,13 @@
+using Store.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
+
+app.MapControllers();
 
 app.Run();
 
